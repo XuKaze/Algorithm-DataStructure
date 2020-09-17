9667b62067f87644218c60f17f2e7c0c7669922f
XuKaze <46765635+XuKaze@users.noreply.github.com>
2020-09-17 11:19:53 +1000

    1

--- BFS/Tree.py
+++ BFS/Tree.py
@@ -9,25 +9,42 @@ class Tree:
         self.root = None
         self.queue = []
         self.nodeList = nodeList
+        self.structure = []
 
     def construct(self):
-        for item in nodeList:
+        for item in self.nodeList:
             node = TreeNode(item)
             if self.root is None:
                 self.root = node
+                self.structure.append(node)
                 self.queue.append(self.root)
             else:
                 treenode = self.queue[0]
                 if treenode.left is None:
+                    self.structure.append(node)
                     treenode.left = node
                     self.queue.append(treenode.left)
                 else:
                     treenode.right = node
+                    self.structure.append(node)
                     self.queue.append(treenode.right)
                     self.queue.pop(0)
 
+    def bfs(self):
+        order = []
+        if self.root is None:
+            return order
 
-
-node_List = [5,1,4,None,None,3,6]
-
-A = Tree(node_List)
+        q = []
+        q.append(self.root)
+        while len(q) != 0:
+            length = len(q)
+            for i in range(length):
+                r = q.pop(0)
+                if r.left is not None:
+                    q.append(r.left)
+                if r.right() is Not None:
+                    q.append(r.right)
+                order.append(r.val)
+                print(r.val)
+        return order
